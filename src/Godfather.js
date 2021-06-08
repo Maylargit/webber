ec0dd8b897c3bfea50485cf0c720df8d23a5e656
Akintade Ajibade <ajibade.akintade@gmail.com>
2021-06-08 18:49:51 +0100

    added styled components

---
+++ src/Godfather.js
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+
+const Godfather = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <div>Godfather</div>
+    </ThemeProvider>
+  );
+};
+
+export default Godfather;
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  color: {
+    red: {
+      main: "#7a0e25",
+      light: "#853c4f",
+    },
+    black: {
+      main: "#000000",
+      light: "#0a0a0a",
+    },
+    grey: {
+      main: "#737373",
+      light: "#7f7f7f",
+      background: "#858585",
+    },
+  },
+};
